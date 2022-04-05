5950895ebb43997290f11a8bda3471bdb4bc0e4e
Kate Montgomery <katemontgomery431@gmail.com>
2022-04-04 22:03:00 -0400

    commit tree 443d26a33028f0b913a1bfe8384476e8327310d0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(name) {
+    bestCustomer = name;
+}
+const leastFavoruteCustomer = 'probably bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoruteCustomer = 'definetly bob';
+}
